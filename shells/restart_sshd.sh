be62b215170fd62580286c21fa6e3c22ab5903b4
LANDAN <landanhsu@bpit888.com>
2018-12-22 05:51:37 +0800

    shell

--- shells/restart_sshd.sh
+++ shells/restart_sshd.sh
@@ -8,4 +8,4 @@ echo "================================"
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo service sshd restart
 touch /home/vagrant/.ssh/authorized_keys
-echo $ssh_key >> /home/vagrant/.ssh/authorized_keys
+echo $ssh_key > /home/vagrant/.ssh/authorized_keys
