4038b5155e33ad266fca00dada9500e31bfbd9f7
LANDAN <landanhsu@bpit888.com>
2019-01-07 09:16:20 +0800

    Fixed: key

--- linux_shells/echo-id_rsa-to-ssh.sh
+++ linux_shells/echo-id_rsa-to-ssh.sh
@@ -19,11 +19,12 @@ sudo touch /home/vagrant/.ssh/id_rsa.pub
 sudo touch /root/.ssh/id_rsa
 sudo touch /root/.ssh/id_rsa.pub
 
-sudo echo $id_rsa > /home/vagrant/.ssh/id_rsa
-sudo echo $id_rsa_pub > /home/vagrant/.ssh/id_rsa.pub
+# 必须双引号， id_ras 才保留换行符号
+sudo echo "$id_rsa" > /home/vagrant/.ssh/id_rsa
+sudo echo "$id_rsa_pub" > /home/vagrant/.ssh/id_rsa.pub
 
-sudo echo $id_rsa > /root/.ssh/id_rsa
-sudo echo $id_rsa_pub > /root/.ssh/id_rsa.pub
+sudo echo "$id_rsa" > /root/.ssh/id_rsa
+sudo echo "$id_rsa_pub" > /root/.ssh/id_rsa.pub
 
 sudo chmod -R 700 /home/vagrant/.ssh/
 sudo chmod -R 700 /root/.ssh/
