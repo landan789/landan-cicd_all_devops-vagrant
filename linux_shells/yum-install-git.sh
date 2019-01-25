46ca72716a0a5e842e38686270cf0aedffda7967
LANDAN <landanhsu@bpit888.com>
2019-01-25 16:51:09 +0800

    mysql ip

---
+++ linux_shells/yum-install-git.sh
@@ -0,0 +1,11 @@
+sudo yum install -y epel-release
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm 
+sudo yum install -y git2u
+
+git clone git@github.com:landan789/landan-cicd_devops-ansible.git /home/vagrant/www
+
+# sudo sh /home/vagrant/www/landan-cicd_devops-ansible/linux_shells/yum-install-ansible.sh
+
+# ansbible-playbooks /home/vagrant/www/landan-cicd_devops-ansible/playbooks/php.yml
+# ansbible-playbooks /home/vagrant/www/landan-cicd_devops-ansible/playbooks/mysql.yml
+# ansbible-playbooks /home/vagrant/www/landan-cicd_devops-ansible/playbooks/nginx.yml
